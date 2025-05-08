db8e3709a6c6a508ad1b8497dcc4da26b525fcc5
Camilo Marin <camilogame2018@hotmail.com>
2025-05-07 19:02:52 -0500

    cambio rapido

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,16 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-  const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-  const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-  export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    fetch: (url, options = {}) => {
-      return fetch(url, {
-        ...options,
-        headers: {
-          ...options.headers,
-          Accept: "application/json",
-        },
-      });
-    },
-  });
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
